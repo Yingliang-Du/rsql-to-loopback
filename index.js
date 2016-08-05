1e1e63afb55baccd863431e30e3757076e66b1cb
Yingliang Du <yingliang_d@yahoo.com>
2016-08-05 12:26:45 -0400

    move rsql-to-loopback class from node-rsql-parser to rsql-to-loopback project

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
 module.exports = {
-	convert: require('node-rsql-parser/src/rsql-to-loopback'),
+	convert: require('./src/rsql-to-loopback'),
 	convert4keys: require('./src/rsql-to-loopback-for-keys')
 };
